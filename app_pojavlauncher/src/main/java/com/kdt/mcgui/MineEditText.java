441c655f380e9ba9387c7e8dc174331948ba5c58
KiuaA1 <aakiua4@gmail.com>
2026-09-24 22:09:50 +0300

    ui: remove remaining legacy mcgui styling

--- app_pojavlauncher/src/main/java/com/kdt/mcgui/MineEditText.java
+++ app_pojavlauncher/src/main/java/com/kdt/mcgui/MineEditText.java
@@ -2,22 +2,16 @@ package com.kdt.mcgui;
 
 import android.content.*;
 import android.util.*;
-import android.graphics.*;
-import android.widget.EditText;
+import androidx.core.content.res.ResourcesCompat;
+import git.artdeell.mojo.R;
 
 public class MineEditText extends androidx.appcompat.widget.AppCompatEditText {
-	public MineEditText(Context ctx) {
-		super(ctx);
-		init();
-	}
-
-	public MineEditText(Context ctx, AttributeSet attrs) {
-		super(ctx, attrs);
-		init();
-	}
-
-	public void init() {
-		setBackgroundColor(Color.parseColor("#131313"));
-		setPadding(5, 5, 5, 5);
-	}
+    public MineEditText(Context ctx) { super(ctx); init(); }
+    public MineEditText(Context ctx, AttributeSet attrs) { super(ctx, attrs); init(); }
+    public void init() {
+        setBackground(ResourcesCompat.getDrawable(getResources(), R.drawable.a_input, getContext().getTheme()));
+        setTextColor(getResources().getColor(R.color.primary_text));
+        setHintTextColor(getResources().getColor(R.color.secondary_text));
+        setPadding(getResources().getDimensionPixelSize(R.dimen._12sdp), getResources().getDimensionPixelSize(R.dimen.padding_input_top), getResources().getDimensionPixelSize(R.dimen._12sdp), getResources().getDimensionPixelSize(R.dimen.padding_input_bottom));
+    }
 }
