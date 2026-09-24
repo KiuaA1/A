7d243552e800de3b4790aaaba796637b312a82a8
KiuaA1 <aakiua4@gmail.com>
2026-09-24 22:23:04 +0300

    Align MineButton text with A theme

--- app_pojavlauncher/src/main/java/com/kdt/mcgui/MineButton.java
+++ app_pojavlauncher/src/main/java/com/kdt/mcgui/MineButton.java
@@ -22,6 +22,8 @@ public class MineButton extends androidx.appcompat.widget.AppCompatButton {
 	public void init() {
 		setTypeface(ResourcesCompat.getFont(getContext(), R.font.noto_sans_bold));
 		setBackground(ResourcesCompat.getDrawable(getResources(), R.drawable.mine_button_background, null));
+		setTextColor(ResourcesCompat.getColor(getResources(), R.color.primary_text, getContext().getTheme()));
+		setAllCaps(false);
 		setTextSize(TypedValue.COMPLEX_UNIT_PX, getResources().getDimensionPixelSize(R.dimen._13ssp));
 	}
 
