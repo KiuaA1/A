11f0203b862f6aeca57943c17d7a93435b961512
KiuaA1 <aakiua4@gmail.com>
2026-09-24 22:16:17 +0300

    Apply A Library styling to preference rows

--- app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/prefs/screens/LauncherPreferenceFragment.java
+++ app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/prefs/screens/LauncherPreferenceFragment.java
@@ -5,12 +5,18 @@ import android.Manifest;
 import android.app.Activity;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.graphics.Rect;
+import android.graphics.drawable.Drawable;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.preference.Preference;
 import androidx.preference.PreferenceFragmentCompat;
+import androidx.recyclerview.widget.RecyclerView;
 
 import net.kdt.pojavlaunch.LauncherActivity;
 import git.artdeell.mojo.R;
@@ -25,8 +31,64 @@ public class LauncherPreferenceFragment extends PreferenceFragmentCompat impleme
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        view.setBackgroundResource(R.drawable.a_background);
         super.onViewCreated(view, savedInstanceState);
+        view.setBackgroundResource(R.drawable.a_background);
+
+        RecyclerView recyclerView = view.findViewById(androidx.preference.R.id.recycler_view);
+        if (recyclerView == null) return;
+
+        recyclerView.setBackgroundResource(R.drawable.a_background);
+        recyclerView.setClipToPadding(false);
+        int horizontal = getResources().getDimensionPixelSize(R.dimen._12sdp);
+        int vertical = getResources().getDimensionPixelSize(R.dimen._8sdp);
+        recyclerView.setPadding(horizontal, vertical, horizontal, vertical);
+        recyclerView.addItemDecoration(new RecyclerView.ItemDecoration() {
+            @Override
+            public void getItemOffsets(@NonNull Rect outRect, @NonNull View child,
+                                        @NonNull RecyclerView parent, @NonNull RecyclerView.State state) {
+                outRect.bottom = vertical;
+            }
+        });
+
+        recyclerView.addOnChildAttachStateChangeListener(new RecyclerView.OnChildAttachStateChangeListener() {
+            @Override public void onChildViewAttachedToWindow(@NonNull View child) {
+                stylePreferenceRow(child);
+            }
+            @Override public void onChildViewDetachedFromWindow(@NonNull View child) { }
+        });
+        for (int i = 0; i < recyclerView.getChildCount(); i++) {
+            stylePreferenceRow(recyclerView.getChildAt(i));
+        }
+    }
+
+    private void stylePreferenceRow(View row) {
+        if (row.findViewById(android.R.id.title) == null) return;
+        row.setBackgroundResource(R.drawable.a_list_item);
+        int horizontal = getResources().getDimensionPixelSize(R.dimen._14sdp);
+        int vertical = getResources().getDimensionPixelSize(R.dimen._10sdp);
+        row.setPadding(horizontal, vertical, horizontal, vertical);
+        stylePreferenceChildren(row);
+    }
+
+    private void stylePreferenceChildren(View view) {
+        if (view instanceof TextView) {
+            TextView textView = (TextView) view;
+            if (textView.getId() == android.R.id.summary) {
+                textView.setTextColor(requireContext().getColor(R.color.secondary_text));
+            } else if (textView.getId() == android.R.id.title || textView.getId() == R.id.seekbar_value) {
+                textView.setTextColor(requireContext().getColor(R.color.primary_text));
+            }
+        } else if (view instanceof ImageView) {
+            ImageView imageView = (ImageView) view;
+            Drawable drawable = imageView.getDrawable();
+            if (drawable != null) drawable.setTint(requireContext().getColor(R.color.primary_text));
+        }
+        if (view instanceof ViewGroup) {
+            ViewGroup group = (ViewGroup) view;
+            for (int i = 0; i < group.getChildCount(); i++) {
+                stylePreferenceChildren(group.getChildAt(i));
+            }
+        }
     }
 
     @Override
