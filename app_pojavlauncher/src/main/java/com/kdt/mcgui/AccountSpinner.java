b450aa4776d31679f6302e6639c500298e84895b
KiuaA1 <aakiua4@gmail.com>
2026-09-24 22:08:15 +0300

    ui: keep account spinner on A pill styling

--- app_pojavlauncher/src/main/java/com/kdt/mcgui/AccountSpinner.java
+++ app_pojavlauncher/src/main/java/com/kdt/mcgui/AccountSpinner.java
@@ -128,7 +128,7 @@ public class AccountSpinner extends AppCompatSpinner implements LoginListener, A
         setOnItemSelectedListener(this);
         reload();
 
-        setBackgroundColor(getResources().getColor(R.color.background_status_bar));
+        setBackground(ResourcesCompat.getDrawable(getResources(), R.drawable.a_pill, getContext().getTheme()));
         mLoginBarPaint.setColor(getResources().getColor(R.color.minebutton_color));
         mLoginBarPaint.setStrokeWidth(getResources().getDimensionPixelOffset(R.dimen._2sdp));
         mLoginStepAnimator.addUpdateListener(this);
