0af0c9b6828a173f001a526546f68df7505f60de
KiuaA1 <aakiua4@gmail.com>
2026-09-24 22:44:20 +0300

    Style delete confirmation with A dialog

--- app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/fragments/DeleteConfirmDialogFragment.java
+++ app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/fragments/DeleteConfirmDialogFragment.java
@@ -23,7 +23,7 @@ public class DeleteConfirmDialogFragment extends DialogFragment {
     @Override
     public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
         if (mInstance == null) dismiss();
-        return new AlertDialog.Builder(requireContext())
+        AlertDialog dialog = new AlertDialog.Builder(requireContext())
                 .setTitle(R.string.instance_delete)
                 .setMessage(R.string.instance_delete_confirmation)
                 .setPositiveButton(R.string.global_delete, (dialog, which) -> {
@@ -38,6 +38,18 @@ public class DeleteConfirmDialogFragment extends DialogFragment {
                 })
                 .setNegativeButton(R.string.global_no, null)
                 .create();
+        dialog.setOnShowListener(d -> {
+            if (dialog.getWindow() != null) dialog.getWindow().setBackgroundDrawableResource(R.drawable.a_dialog);
+            android.widget.TextView title = dialog.findViewById(androidx.appcompat.R.id.alertTitle);
+            android.widget.TextView message = dialog.findViewById(android.R.id.message);
+            if (title != null) title.setTextColor(requireContext().getColor(R.color.primary_text));
+            if (message != null) message.setTextColor(requireContext().getColor(R.color.secondary_text));
+            android.widget.Button positive = dialog.getButton(AlertDialog.BUTTON_POSITIVE);
+            android.widget.Button negative = dialog.getButton(AlertDialog.BUTTON_NEGATIVE);
+            if (positive != null) { positive.setTextColor(requireContext().getColor(R.color.a_accent_text)); positive.setAllCaps(false); }
+            if (negative != null) { negative.setTextColor(requireContext().getColor(R.color.secondary_text)); negative.setAllCaps(false); }
+        });
+        return dialog;
     }
     public static String TAG = "delete_dialog_confirm";
 }
